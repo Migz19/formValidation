c8b137ba1a40fe96d87ad207e20c3dcde031d048
Ahmed Magdy <ahmedmagdy373@gmail.com>
2023-03-08 15:48:47 +0200

    Form Validation with Regex

--- lib/src/model/user_model.dart
+++ lib/src/model/user_model.dart
@@ -4,16 +4,19 @@ class UserModel {
   final String _message;
   final bool _isOnline;
   String _imageUri;
+  final String _email;
 
   UserModel(
       {required name,
-      required message,
-      required isOnline,
+       message,
+       isOnline,
+         email,
       imageUri =
           "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
       })
       : _message = message,
         _name = name,
+        _email=email,
         _imageUri = imageUri,
         _isOnline = isOnline;
 
